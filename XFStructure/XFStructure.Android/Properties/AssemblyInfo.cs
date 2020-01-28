73f73f2f7bfc6059c62e3eee53f1e3f7f1ebe484
Adeed Khalid <adeed.khalid@aciano.net>
2020-01-28 12:13:44 +0500

    Commit - Android & iOS Project Placed in 'Plaforms' Folder

--- XFStructure/XFStructure.Android/Properties/AssemblyInfo.cs
+++ XFStructure/XFStructure.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XFStructure.Android")]
+[assembly: AssemblyTitle("Platforms.XFStructure.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XFStructure.Android")]
+[assembly: AssemblyProduct("Platforms.XFStructure.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- XFStructure/XFStructure.iOS/Properties/AssemblyInfo.cs
+++ XFStructure/XFStructure.iOS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XFStructure.iOS")]
+[assembly: AssemblyTitle("Platforms.XFStructure.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XFStructure.iOS")]
+[assembly: AssemblyProduct("Platforms.XFStructure.iOS")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
