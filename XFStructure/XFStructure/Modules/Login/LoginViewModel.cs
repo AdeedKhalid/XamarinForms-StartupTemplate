fc1c43bfcb79e336333e820b1728f04de3ffca99
Adeed Khalid <adeed.khalid@aciano.net>
2020-01-29 18:30:09 +0500

    Commit - Implemented MVVM's ICommand & Navigations

--- XFStructure/XFStructure/Modules/Login/LoginViewModel.cs
+++ XFStructure/XFStructure/Modules/Login/LoginViewModel.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 using Xamvvm;
+using XFStructure.Modules.Signup;
 
 namespace XFStructure.Modules.Login
 {
     public class LoginViewModel : BasePageModel
     {
+        #region Properties
+
+        #endregion
+
+
+        #region Commands
+        private ICommand _navigateToSignup;
+        public ICommand NavigateToSignup => _navigateToSignup ?? (_navigateToSignup = new Command(ExecuteNavigateToSignupCommand));
+        #endregion
+
+
+        #region Methods
+        private async void ExecuteNavigateToSignupCommand(object obj)
+        {
+            var param = obj as string;
+            switch (param)
+            {
+                case "NavigateToSignup":
+                    await this.PushPageFromCacheAsync<SignupViewModel>();
+                    break;
+            }
+        }
+
         public LoginViewModel()
         {
 
         }
+        #endregion
+
     }
 }
--- XFStructure/XFStructure/Modules/Signup/SignupViewModel.cs
+++ XFStructure/XFStructure/Modules/Signup/SignupViewModel.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 using Xamvvm;
 
 namespace XFStructure.Modules.Signup
 {
     public class SignupViewModel : BasePageModel
     {
+        #region Properties
+
+        #endregion
+
+
+        #region Commands
+        private ICommand _navigateToSignin;
+        public ICommand NavigateToSignin => _navigateToSignin ?? (_navigateToSignin = new Command(ExecuteNavigateToSigninCommand));
+        #endregion
+
+
+        #region Methods
+        private async void ExecuteNavigateToSigninCommand(object obj)
+        {
+            var param = obj as string;
+            switch (param)
+            {
+                case "NavigateToSignin":
+                    await this.PopPageAsync();
+                    break;
+            }
+        }
         public SignupViewModel()
         {
 
         }
+        #endregion
     }
 }
