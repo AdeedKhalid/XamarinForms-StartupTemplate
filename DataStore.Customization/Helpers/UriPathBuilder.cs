61761ff9fbec38fa4dca6a61d49ab86213486ad1
Adeed Khalid <adeed.khalid@aciano.net>
2020-08-08 17:39:56 +0500

    Commit - Url Path & Query Builder Added

---
+++ DataStore.Customization/Helpers/UriPathBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataStore.Customization.Helpers
+{
+    public class UriPathBuilder
+    {
+        private static UriBuilder _uriBuilder;
+
+        public UriPathBuilder() { }
+
+        public UriPathBuilder(string endPoint) => _uriBuilder = new UriBuilder(endPoint);
+
+        public static UriBuilder GetPath(string path, UriQueryBuilder queryBuilder = null)
+        {
+            if (_uriBuilder == null) _uriBuilder = new UriBuilder(path);
+            //_uriBuilder.Path = path;
+            _uriBuilder.Query = queryBuilder?.Build();
+            return _uriBuilder;
+        }
+    }
+}
