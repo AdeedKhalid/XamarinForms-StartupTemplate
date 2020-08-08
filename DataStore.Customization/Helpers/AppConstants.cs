7df4d74c3e16f1b387e4c2cffae7328c36a66283
Adeed Khalid <adeed.khalid@aciano.net>
2020-08-08 17:42:01 +0500

    Commit - AppConstants.cs Added

---
+++ DataStore.Customization/Helpers/AppConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStore.Customization.Helpers
+{
+    public static class AppConstants
+    {
+        public struct StringKeys
+        {
+            public const string Username = "Username";
+            public const string Password = "Password";
+            public const string DeviceId = "DeviceId";
+        }
+
+        public struct StoreLinks
+        {
+            public const string Playstore = "https://play.google.com/store/apps/details?id=com.techlogix.mobilinkcustomer";
+            public const string Appstore = "https://itunes.apple.com/pk/app/jazzcash/id1224617688?mt=8";
+        }
+    }
+}
